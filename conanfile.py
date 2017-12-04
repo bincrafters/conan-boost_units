56643fb595d80887118c8944bd183b072c9c7a3f
Rene Rivera <grafikrobot@gmail.com>
2017-12-03 23:37:31 -0600

    Next generation of Boost packages.

--- conanfile.py
+++ conanfile.py
@@ -1,42 +1,48 @@
 from conans import ConanFile, tools
-import os
+
 
 class BoostUnitsConan(ConanFile):
     name = "Boost.Units"
     version = "1.65.1"
-    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-units"
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
+
+    requires = \
+        "Boost.Assert/1.65.1@bincrafters/testing", \
+        "Boost.Config/1.65.1@bincrafters/testing", \
+        "Boost.Core/1.65.1@bincrafters/testing", \
+        "Boost.Integer/1.65.1@bincrafters/testing", \
+        "Boost.Io/1.65.1@bincrafters/testing", \
+        "Boost.Lambda/1.65.1@bincrafters/testing", \
+        "Boost.Math/1.65.1@bincrafters/testing", \
+        "Boost.Mpl/1.65.1@bincrafters/testing", \
+        "Boost.Preprocessor/1.65.1@bincrafters/testing", \
+        "Boost.Serialization/1.65.1@bincrafters/testing", \
+        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
+        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
+        "Boost.Typeof/1.65.1@bincrafters/testing"
+
     lib_short_names = ["units"]
-    requires =  "Boost.Assert/1.65.1@bincrafters/testing", \
-                      "Boost.Config/1.65.1@bincrafters/testing", \
-                      "Boost.Core/1.65.1@bincrafters/testing", \
-                      "Boost.Integer/1.65.1@bincrafters/testing", \
-                      "Boost.Io/1.65.1@bincrafters/testing", \
-                      "Boost.Lambda/1.65.1@bincrafters/testing", \
-                      "Boost.Math/1.65.1@bincrafters/testing", \
-                      "Boost.Mpl/1.65.1@bincrafters/testing", \
-                      "Boost.Preprocessor/1.65.1@bincrafters/testing", \
-                      "Boost.Serialization/1.65.1@bincrafters/testing", \
-                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-                      "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-                      "Boost.Typeof/1.65.1@bincrafters/testing"
+    is_header_only = True
 
-                      #assert1 config0 core2 integer3 io1 lambda6 math8 mpl5 preprocessor0 serialization11 static_assert1 type_traits3 typeof5
-                      
-    def source(self):
-        boostorg_github = "https://github.com/boostorg"
-        archive_name = "boost-" + self.version  
-        for lib_short_name in self.lib_short_names:
-            tools.get("{0}/{1}/archive/{2}.tar.gz"
-                .format(boostorg_github, lib_short_name, archive_name))
-            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
+    # BEGIN
 
-    def package(self):
-        for lib_short_name in self.lib_short_names:
-            include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)		
+    short_paths = True
+    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
 
     def package_id(self):
-        self.info.header_only()
+        if self.is_header_only:
+            self.info.header_only()
+
+    @property
+    def env(self):
+        try:
+            with tools.pythonpath(super(self.__class__, self)):
+                import boostgenerator # pylint: disable=F0401
+                boostgenerator.BoostConanFile(self)
+        except:
+            pass
+        return super(self.__class__, self).env
+
+    # END

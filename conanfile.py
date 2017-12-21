d7ebd6d1555f91d3d4bb4f0a85104883fe471ab8
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:36 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,22 +3,22 @@ from conans import ConanFile
 
 class BoostUnitsConan(ConanFile):
     name = "Boost.Units"
-    version = "1.65.1"
+    version = "1.66.0"
 
     requires = \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Integer/1.65.1@bincrafters/testing", \
-        "Boost.Io/1.65.1@bincrafters/testing", \
-        "Boost.Lambda/1.65.1@bincrafters/testing", \
-        "Boost.Math/1.65.1@bincrafters/testing", \
-        "Boost.Mpl/1.65.1@bincrafters/testing", \
-        "Boost.Preprocessor/1.65.1@bincrafters/testing", \
-        "Boost.Serialization/1.65.1@bincrafters/testing", \
-        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-        "Boost.Typeof/1.65.1@bincrafters/testing"
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Integer/1.66.0@bincrafters/testing", \
+        "Boost.Io/1.66.0@bincrafters/testing", \
+        "Boost.Lambda/1.66.0@bincrafters/testing", \
+        "Boost.Math/1.66.0@bincrafters/testing", \
+        "Boost.Mpl/1.66.0@bincrafters/testing", \
+        "Boost.Preprocessor/1.66.0@bincrafters/testing", \
+        "Boost.Serialization/1.66.0@bincrafters/testing", \
+        "Boost.Static_Assert/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing", \
+        "Boost.Typeof/1.66.0@bincrafters/testing"
 
     lib_short_names = ["units"]
     is_header_only = True
@@ -26,9 +26,9 @@ class BoostUnitsConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-units"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 

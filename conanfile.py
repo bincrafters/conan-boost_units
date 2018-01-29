ba9d8d42da7620a6afc46d5c51d3f23856fefda2
Rene Rivera <grafikrobot@gmail.com>
2018-01-29 09:53:49 -0600

    Merge.

--- conanfile.py
+++ conanfile.py
@@ -17,20 +17,20 @@ class BoostUnitsConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/stable",
-        "boost_assert/1.66.0@bincrafters/stable",
-        "boost_config/1.66.0@bincrafters/stable",
-        "boost_core/1.66.0@bincrafters/stable",
-        "boost_integer/1.66.0@bincrafters/stable",
-        "boost_io/1.66.0@bincrafters/stable",
-        "boost_lambda/1.66.0@bincrafters/stable",
-        "boost_math/1.66.0@bincrafters/stable",
-        "boost_mpl/1.66.0@bincrafters/stable",
-        "boost_preprocessor/1.66.0@bincrafters/stable",
-        "boost_serialization/1.66.0@bincrafters/stable",
-        "boost_static_assert/1.66.0@bincrafters/stable",
-        "boost_type_traits/1.66.0@bincrafters/stable",
-        "boost_typeof/1.66.0@bincrafters/stable"
+        "boost_package_tools/1.66.0@bincrafters/testing",
+        "boost_assert/1.66.0@bincrafters/testing",
+        "boost_config/1.66.0@bincrafters/testing",
+        "boost_core/1.66.0@bincrafters/testing",
+        "boost_integer/1.66.0@bincrafters/testing",
+        "boost_io/1.66.0@bincrafters/testing",
+        "boost_lambda/1.66.0@bincrafters/testing",
+        "boost_math/1.66.0@bincrafters/testing",
+        "boost_mpl/1.66.0@bincrafters/testing",
+        "boost_preprocessor/1.66.0@bincrafters/testing",
+        "boost_serialization/1.66.0@bincrafters/testing",
+        "boost_static_assert/1.66.0@bincrafters/testing",
+        "boost_type_traits/1.66.0@bincrafters/testing",
+        "boost_typeof/1.66.0@bincrafters/testing"
     )
 
     # BEGIN
@@ -38,7 +38,7 @@ class BoostUnitsConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/stable"
+    build_requires = "boost_generator/1.66.0@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
